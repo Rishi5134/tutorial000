b49218bd1665de64d7367d68f7d82761f2e851d3
Rishi Rathod <rishi3rathod5@gmail.com>
2022-09-20 09:33:46 +0530

    deploy demo day2 try002

--- web/index.js
+++ web/index.js
@@ -43,7 +43,7 @@ Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
 });
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
-// See the ensureBilling helper to learn more about billing in this template.
+// See the ensureBilling helper to learn more about billing in this temsdfplate.
 const BILLING_SETTINGS = {
   required: false,
   // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
