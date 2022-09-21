8f8aa8e809522202d3a4f06c3d4729f0798f3c6f
Rishi Rathod <rishi3rathod5@gmail.com>
2022-09-21 11:12:01 +0530

    deploy test

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -30,25 +30,7 @@ export default function HomePage() {
               <Stack.Item fill>
                 <TextContainer spacing="loose">
                   <Heading>Nice work on building a Shopify app 🎉</Heading>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
+                 
                   <p>
                     Ready to go? Start populating your app with some sample
                     products to view and test in your store.{" "}
